ece461dd4b8de5aeb8561bffe0131e9d895cbc17
syafiqazyan <syafiq.azyan.95@gmail.com>
2023-09-14 11:07:01 +0800

    Update add_new.php
    put the coding inside the if bracket

--- add_new.php
+++ add_new.php
@@ -2,23 +2,24 @@
 
   include "db_conn.php";
 
-  if(isset($_POST['submit'])){
+  if(isset($_POST['submit'])) {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $gender = $_POST['gender'];
+
+    $sql = "INSERT INTO `crud`(`id`, `first_name`, `last_name`, `email`, `gender`) VALUES (NULL,'$first_name','$last_name','$email','$gender')";
+
+    $result = mysqli_query($conn, $sql);
+  
+    if($result) {
+      header("Location: index.php?msg=New record created successfully");
+    }
+    else {
+      echo "Failed: " . mysqli_error($conn);
+    }
   }
 
-  $sql = "INSERT INTO `crud`(`id`, `first_name`, `last_name`, `email`, `gender`) VALUES (NULL,'$first_name','$last_name','$email','$gender')";
-
-  $result = mysqli_query($conn, $sql);
-
-  if($result) {
-    header("Location: index.php?msg=New record created successfully");
-  }
-  else {
-    echo "Failed: " . mysqli_error($conn);
-  }
 
 ?>
 
