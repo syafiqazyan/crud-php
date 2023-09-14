cef976e2118952f136caa745374752be61cb8f8a
syafiqazyan <syafiq.azyan.95@gmail.com>
2023-09-14 10:38:48 +0800

    Update add_new.php
    adding firstname, lastname, email.

--- add_new.php
+++ add_new.php
@@ -3,10 +3,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PHP CRUD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!--font awesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>PHP CRUD</title>
   </head>
   <body>
     <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #00ff5573;">
@@ -18,7 +18,28 @@
         <h3>Add New User</h3>
         <p class="text-muted">Complete the form below to add new user</p>
       </div>
-      <div class="container d-flex "></div>
+      <div class="container d-flex justify-content-center">
+        <form action="" method="post" style="width:50vw; min-width:300px;">
+          <div class="row mb-3">
+            <div class="col">
+              <label for="" class="form-label">First Name:</label>
+              <input type="text" class="form-control" name="first_name" placeholder="Albert">
+            </div>
+            <div class="col">
+              <label for="" class="form-label">Last Name:</label>
+              <input type="text" class="form-control" name="last_name" placeholder="Einstein">
+            </div>
+          </div>
+
+          <div>
+            <div class="mb-3">
+              <label for="" class="form-label">Email:</label>
+              <input type="email" class="form-control" name="email" placeholder="name@example.com">
+            </div>
+          </div>
+
+        </form>
+      </div>
     </div>
 
     <!--Bootstraps-->
